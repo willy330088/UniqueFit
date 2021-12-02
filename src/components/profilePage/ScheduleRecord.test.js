3a9e3219157def299a85af433e9bafa1cc0794e0
Williamlin308 <williamlin330088@gmail.com>
2021-12-02 21:45:05 +0800

    Complete schedule record unit test

--- src/components/profilePage/ScheduleRecord.test.js
+++ src/components/profilePage/ScheduleRecord.test.js
@@ -1,17 +1,17 @@
 import { getRecentEvents } from './ScheduleRecord';
 import moment from 'moment';
 
+function createTime(day, past) {
+  return past
+    ? new Date().getTime() - day * 86400000
+    : new Date().getTime() + day * 86400000;
+}
+
 describe('Testing getRecentEvents()', () => {
   it('should sort data from near date to far date', () => {
-    const date1 = moment(new Date().getTime() - 10 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date2 = moment(new Date().getTime() - 8 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date3 = moment(new Date().getTime() - 5 * 86400000).format(
-      'YYYY-MM-DD'
-    );
+    const date1 = moment(createTime(10, true)).format('YYYY-MM-DD');
+    const date2 = moment(createTime(8, true)).format('YYYY-MM-DD');
+    const date3 = moment(createTime(5, true)).format('YYYY-MM-DD');
     const testEvents = [
       {
         extendedProps: { completed: true },
@@ -46,15 +46,9 @@ describe('Testing getRecentEvents()', () => {
   });
 
   it('should exclude events with future date', () => {
-    const date1 = moment(new Date().getTime() + 10 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date2 = moment(new Date().getTime() - 10 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date3 = moment(new Date().getTime() + 5 * 86400000).format(
-      'YYYY-MM-DD'
-    );
+    const date1 = moment(createTime(10, false)).format('YYYY-MM-DD');
+    const date2 = moment(createTime(10, true)).format('YYYY-MM-DD');
+    const date3 = moment(createTime(5, false)).format('YYYY-MM-DD');
     const testEvents = [
       {
         extendedProps: { completed: true },
@@ -80,15 +74,9 @@ describe('Testing getRecentEvents()', () => {
   });
 
   it('should exclude events with date past than 30 days', () => {
-    const date1 = moment(new Date().getTime() - 5 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date2 = moment(new Date().getTime() - 31 * 86400000).format(
-      'YYYY-MM-DD'
-    );
-    const date3 = moment(new Date().getTime() - 32 * 86400000).format(
-      'YYYY-MM-DD'
-    );
+    const date1 = moment(createTime(5, true)).format('YYYY-MM-DD');
+    const date2 = moment(createTime(31, true)).format('YYYY-MM-DD');
+    const date3 = moment(createTime(32, true)).format('YYYY-MM-DD');
     const testEvents = [
       {
         extendedProps: { completed: true },
